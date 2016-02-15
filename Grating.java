a17d38a5d121e006e4c485aafc91675c869dedc7
Marcel Mueller <mmueller@physik.uni-bielefeld.de>
2016-02-15 22:51:28 +0100

    Candidate selection and direction combination impl.

---
+++ Grating.java
@@ -0,0 +1,115 @@
+/*
+This file is part of fastSIM Grating Search, ported to Java.
+
+This code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+The code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the code.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+package de.bio_photonics.gratingsearch;
+
+/** Structure hold all parameters of a grating. 
+The original MATLAB code can be found here, please cite their
+publication if you use this software to create SIM gratings:
+
+https://github.com/nanoimaging/fastSIM_GratingSearchforSLM
+*/
+class Grating {
+
+    final int ax,ay,bx,by;	// integer pxl size and inc. of grating
+    final double gratDir;	// orientation of grating in rad
+    final double gratPer;	// grating period, in pxl
+    int shiftDir = -1;		// 0-horizontal, 1-vertical, -1 undef.
+
+    /** create and calculate grating from pxl sizes */
+    Grating(int iax, int iay, int ibx, int iby) {
+	ax = iax; ay = iay; bx = ibx; by = iby;
+	
+	gratDir = Math.atan2(ay, ax);
+	double theta	= Math.atan2( by, bx ) ;
+	gratPer =	Math.hypot( bx, by ) * 
+		    Math.abs( Math.sin( gratDir-theta ));
+    }
+
+
+    /** Test if a horizontal shift can provide n equi-distant
+     *  phases. 
+     *  @param nPhases Number of equi-distant phases to check */
+    boolean testStepHorizontal( final int nPhases ) {
+
+	// no shift for horizontal grating
+	if (ay==0) return false;
+	// standard shifts for vertical grating
+	if (by==0) return ( bx % nPhases == 0 );
+
+	int ax2 = ax/gcd(ax, ay);
+	int ay2 = ay/gcd(ax, ay);
+
+	return ( lcm(abs(ay2),abs(by))*(ax2/ay2-bx/by) % nPhases == 0 );
+
+    }
+
+    /** Test if a vertical shift can provide n equi-distant
+     *  phases. 
+     *  @param nPhases Number of equi-distant phases to check */
+    boolean testStepVertical( final int nPhases ) {
+
+	// no shift for horizontal grating
+	if (ax==0) return false;
+	// standard shifts for vertical grating
+	if (bx==0) return ( by % nPhases == 0 );
+
+	int ax2 = ax/gcd(ax, ay);
+	int ay2 = ay/gcd(ax, ay);
+
+	return ( lcm(abs(ax2),abs(bx))*(ay2/ax2-by/bx) % nPhases == 0 );
+    }
+
+
+    /** returns true if the pattern direction is within given range */
+    boolean testDirection(double a1, double a2) {
+	double d = wrapPi( gratDir );
+	if (a1<=d && d<=a2) return true;
+	return false;
+    }
+
+
+    @Override
+    public String toString() {
+	return String.format(
+	    "ax %3d ay %3d bx %3d by %3d, d: %5.3f a %8.3f (%s)",
+	    ax,ay,bx,by, gratPer, gratDir*180/Math.PI, 
+	    (shiftDir==0)?("h"):("v"));
+    }
+
+    /** Helper: greatest common denominator for int */
+    static int gcd(int a, int b) { return b==0 ? a : gcd(b, a%b); }
+
+    /** Helper: least common multiple */
+    static int lcm(int a, int b) { 
+	return (abs(a) / gcd(a,b))*abs(b); 
+    }
+
+    /** Helper: wrapper around abs */
+    static int abs(int i) { return (i<0)?(-i):(i); };
+
+    /** Helper: wrap direction into 0..pi */
+    static double wrapPi(double x) {
+	if (x>=Math.PI)	return wrapPi(x-Math.PI);
+	if (x<0)	return wrapPi(x+Math.PI);
+	return x;
+    }
+
+
+}
+
+
--- Grating_Search.java
+++ Grating_Search.java
@@ -17,6 +17,9 @@ along with the code.  If not, see <http://www.gnu.org/licenses/>
 
 package de.bio_photonics.gratingsearch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** Java port of fastSIM SLM grating search algorithm.
 The original MATLAB code can be found here, please cite their
 publication if you use this software to create SIM gratings:
@@ -25,75 +28,152 @@ https://github.com/nanoimaging/fastSIM_GratingSearchforSLM
 */
 public class Grating_Search {
 
-
     // search value space
     final int axmax = 28, aymax = 28;
     final int bxmin = 2,  bymin = 2;
     final int bxmax = 28, bymax = 28;
 
-
-    // all defining features of the grating
-    class GratingParam {
-	int ax,ay,bx,by;// integer pxl size and inc. of grating
-	double gratdir;	// orientation of grating in rad
-	double gratper;	// grating period, in pxl
-    }
-
-
-    // main loop creating possible gratings
-    public void calcGrat(
+    // Tolerance when looking for equal direction, in rad
+    final static double tolDir = 1e-5;
+    
+    // 1 - loop creating possible gratings
+    public List<Grating> calcGrat(
 	final double gratPerMin, 
-	final double gratPerMax ) {
+	final double gratPerMax,
+	final int phaseSteps ) {
 
+	// List of grating candidates
+	List<Grating> candidates = new ArrayList<Grating>(1000);
+	
+	// outer loop set: loop grating sizes
 	for (int ax = 0; ax < axmax; ax ++ ) {
-	    System.out.println(String.format("DONE: %d / %d ",ax,axmax));
-
+	    System.out.println(String.format(
+		"DONE: %d / %d, #candidates %d",ax,axmax, 
+		    candidates.size()));
 	    for( int ay = -aymax; ay < aymax; ay++ ) {
 
-		double gratDir = Math.atan2(ay,ax);
-
+		// inner loop set: loop grating shifts
 		for ( int bx =  bxmin; bx < bxmax; bx++ )
 		for ( int by = -bxmax; by < bymax; by++ ) {
 		     
 		    if (Math.abs(by)<bymin) continue;
 
-		    // get the grating period
-		    double gratPer = gratingPeriod(ax, ay, bx, by);
-		    if ( gratPer < gratPerMin || gratPer > gratPerMax )
+		    // create the grating, auto-computes parameters
+		    Grating current = new Grating(ax,ay,bx,by);
+	   
+		    // check if the grating period is within bounds
+		    if ( current.gratPer < gratPerMin || 
+			 current.gratPer > gratPerMax )
 			continue;
 
-
-		    System.out.println("gratPer: "+gratPer);
-
+		    // check if n equid. pha. can be shifted horizontal
+		    if ( current.testStepHorizontal(phaseSteps) ) {
+			current.shiftDir = 0;
+			fuzzyAdd( current, candidates );
+		    }
+		    // check if n equid. pha. can be shifted vertical
+		    if ( current.testStepVertical(phaseSteps) ) {
+			current.shiftDir = 1;
+			fuzzyAdd( current, candidates );
+		    }
 
 
 		}
 	    }
 	}
 
+	return candidates;
+    }
 
-
-
-
+    /** Add a grating only if no grating with similar direction
+     *  is already present */
+    private static boolean fuzzyAdd( 
+	Grating a , List<Grating> candidates ) {
+	for ( Grating b : candidates )
+	    if ( Math.abs( b.gratDir  - a.gratDir ) < tolDir )
+		return false;
+	candidates.add( a );
+	return true;
     }
 
 
-    /** Calculate the grating period */
-    double gratingPeriod( int ax, int ay, int bx, int by ) {
-	double phi	= Math.atan2( ay, ax ) ;
-	double theta	= Math.atan2( by, bx ) ;
-	double res	= Math.hypot( bx, by ) * Math.abs( Math.sin( phi-theta ));
-	return res;
+    
+    // 2 - select matching combinations of directions
+    public List<Grating []> selectDirs( 
+	List<Grating> candidates, 
+	final int nrDirs, final double tolDirection ) {
+
+	double sector = Math.PI/nrDirs;
+
+	List<Grating []> ret = new ArrayList<Grating []>();
+
+	// outer loop, select only dirs in the first sector
+	for ( Grating c : candidates ) {
+	    if (!c.testDirection(0, sector)) continue;
+
+	    // create the array
+	    Grating [] col = new Grating[nrDirs];
+	    col[0] = c;
+
+	    // see if the other directions can be found within limits
+	    for (int i=1; i<nrDirs; i++) {
+		double minDist=tolDirection;
+		for ( Grating d : candidates ) {
+		    double dist =  Math.abs(
+			    Grating.wrapPi(d.gratDir) - sector*i - 
+			    Grating.wrapPi(c.gratDir) );
+		    if ( dist < minDist ) {
+			col[i]  = d;
+			minDist = dist;
+		    }
+		}
+	    }
+
+	    // check if a complete set was found
+	    boolean complete = true;
+	    for ( Grating e : col )
+		if ( e == null ) complete = false;
+
+	    if (complete)
+		ret.add( col );
+
+	}
+
+	return ret;
     }
+
+
+
+
+
+
 
     
     /** main method */
     public static void main( String [] args ) {
 
-	Grating_Search gs = new Grating_Search();
-	gs.calcGrat(3,4);
+	Grating_Search gs  = new Grating_Search();
 
+	// calculate gratings 3.2 .. 3.8 pxl size,
+	// allowing for 3 equi-distant phase shifts
+	System.out.println("-- Compute all candidates --");
+	List<Grating> all = gs.calcGrat(3.2,3.8, 3 );
+
+	// collect pairs of 3 directions
+	System.out.println("-- Compute direction combinations --");
+	List<Grating []> dirs = gs.selectDirs(all, 3, 0.04);
+
+	for ( Grating [] i : dirs ) {
+	    System.out.println("---");
+	    for ( Grating j : i )
+		System.out.println( j.toString());
+	}
+
+	
     }
+
+
+
 
 
 
